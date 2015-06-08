0a10cecffd6776dfebd981653973b24e56aaec39
vladislav-gorbenko <vladis.gorbenko@gmail.com>
2015-06-08 19:46:06 +0300

    add shoulda matchers for dependent => destroy

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Answer, type: :model do
 
 		it {should belong_to(:question)}
 
-		it {should validate_presence_of :body}
-		it {should validate_presence_of :question_id}
+		it {should validate_presence_of(:body)}
+		it {should validate_presence_of(:question_id)}
 		it { should validate_length_of(:body).is_at_least(5).is_at_most(3000) }
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -4,24 +4,25 @@ RSpec.describe Question, type: :model do
 
 		it {should have_many(:answers)}
 
-	  it {should validate_presence_of :title}
-	  it {should validate_presence_of :body}
-		it { should validate_length_of(:title).is_at_least(10).is_at_most(256) }
+	  it {should validate_presence_of(:title)}
+	  it {should validate_presence_of(:body)}
+		it {should validate_length_of(:title).is_at_least(10).is_at_most(256) }
 	  it {should validate_length_of(:body).is_at_least(5).is_at_most(3000) }
+	  it { should have_many(:answers).dependent(:destroy) }
 
 
 
-	describe 'when remove question should remove answers' do
-		let!(:question_1){FactoryGirl.create(:question, title:'Hello world!', body: "How do it say simply?")}
-		let!(:answer_1){FactoryGirl.create(:answer, body: "Try to do it by ruby", question_id: question_1.id)}
-		let!(:answer_2){FactoryGirl.create(:answer, body: "Try to do it by js", question_id: question_1.id)}
+	# describe 'when remove question should remove answers' do
+	# 	let!(:question_1){FactoryGirl.create(:question, title:'Hello world!', body: "How do it say simply?")}
+	# 	let!(:answer_1){FactoryGirl.create(:answer, body: "Try to do it by ruby", question_id: question_1.id)}
+	# 	let!(:answer_2){FactoryGirl.create(:answer, body: "Try to do it by js", question_id: question_1.id)}
 
-		it 'add answers to question' do
-			expect(question_1.answers.count).to eq(2)
-		end
+	# 	it 'add answers to question' do
+	# 		expect(question_1.answers.count).to eq(2)
+	# 	end
 
-		it 'remove question and answers' do
-			expect{question_1.destroy}.to change{Answer.count}.by(-2)
-		end
-	end
+	# 	it 'remove question and answers' do
+	# 		expect{question_1.destroy}.to change{Answer.count}.by(-2)
+	# 	end
+	# end
 end
